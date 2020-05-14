da3f277824dfd277b3c7901f124fba756659730a
“Tommy” <nh3r717@gmail.com>
2020-05-14 08:28:46 +0800

    new day – aligning components / App.js

--- Wk2/wdp5_react/src/Component/StaticHeader.js
+++ Wk2/wdp5_react/src/Component/StaticHeader.js
@@ -9,21 +9,21 @@
 import React from 'react'
 import QRPlaceholder from '../images/QRPlaceholder.svg'
 
-  // Functional Component
-  const Name = props => {
-      return (
-        <div>
-        <img src={QRPlaceholder} alt="QR Code" style={styles.QRCode} />
+// Functional Component
+const Name = props => {
+  return (
+    <div>
+      <h1 style={styles.h1}>QR Code Generator</h1>
     </div>
-      )
-  }
-  export default Name
-  
+  )
+}
+export default Name
+
 const styles = {
-    QRCode: {
-    },
-    element2: {
-        display: 'flex',
-        margin: '1em 0 0 0'
-    }
+  QRCode: {
+  },
+  element2: {
+    display: 'flex',
+    margin: '1em 0 0 0'
+  }
 } 
