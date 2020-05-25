6086146ba2bfb90e52e57b9278a395dcd576873d
“Tommy” <nh3r717@gmail.com>
2020-05-25 09:25:14 +0800

    update to .jsx file extensions

---
+++ Wk3/wdp5_react/src/Component/Test2.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Test2 extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+  
+      this.handleInputChange = this.handleInputChange.bind(this);
+    }
+  
+    handleInputChange(event) {
+
+    }
+  
+    render() {
+      return (
+        <form>
+
+        </form>
+      );
+    }
+}
+  
+export default Test2
