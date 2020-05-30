b54ffb7b00474359021fcca4a662dcb82640c539
“Tommy” <nh3r717@gmail.com>
2020-05-30 19:31:21 +0800

    not live online gh-pages

---
+++ Wk4/wdp5_react/build/precache-manifest.d463f49f51ec88f0322c5f19839f1a49.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeaddd584164db1d381f1a9ede63c21",
+    "url": "/NH3R717/WDP5/index.html"
+  },
+  {
+    "revision": "811a3e29aa1e04e10172",
+    "url": "/NH3R717/WDP5/static/css/main.91a1178a.chunk.css"
+  },
+  {
+    "revision": "a30b3d76a79772f076bc",
+    "url": "/NH3R717/WDP5/static/js/2.8f7d94f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NH3R717/WDP5/static/js/2.8f7d94f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811a3e29aa1e04e10172",
+    "url": "/NH3R717/WDP5/static/js/main.4f4945a5.chunk.js"
+  },
+  {
+    "revision": "9a22a8ec750f07368f32",
+    "url": "/NH3R717/WDP5/static/js/runtime-main.2d0bb07e.js"
+  },
+  {
+    "revision": "d350ce249a30596db0ed98838873a907",
+    "url": "/NH3R717/WDP5/static/media/Comfortaa-Regular.d350ce24.ttf"
+  },
+  {
+    "revision": "72950f711133364a3ba79af4055d21e3",
+    "url": "/NH3R717/WDP5/static/media/QRPlaceholder.72950f71.svg"
+  },
+  {
+    "revision": "79d274c7cb2280246c0e9ab557a31a1f",
+    "url": "/NH3R717/WDP5/static/media/close.79d274c7.svg"
+  },
+  {
+    "revision": "cc3e92b43d95bdc7e046cc6801a89acb",
+    "url": "/NH3R717/WDP5/static/media/info.cc3e92b4.svg"
+  }
+]);
