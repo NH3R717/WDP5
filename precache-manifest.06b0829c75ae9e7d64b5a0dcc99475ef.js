06b6bfcc725b4879f888fd4139311115c7f19e40
“Tommy” <nh3r717@gmail.com>
2020-05-31 09:19:24 +0800

    Updates

---
+++ precache-manifest.06b0829c75ae9e7d64b5a0dcc99475ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8271b956312229b981e0de581e5476c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "060c0a35c737d027e86f",
+    "url": "./static/css/main.9fed9724.chunk.css"
+  },
+  {
+    "revision": "a30b3d76a79772f076bc",
+    "url": "./static/js/2.8f7d94f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8f7d94f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060c0a35c737d027e86f",
+    "url": "./static/js/main.510ed9ae.chunk.js"
+  },
+  {
+    "revision": "e148614149f04547eedc",
+    "url": "./static/js/runtime-main.2745c5dd.js"
+  },
+  {
+    "revision": "d350ce249a30596db0ed98838873a907",
+    "url": "./static/media/Comfortaa-Regular.d350ce24.ttf"
+  },
+  {
+    "revision": "72950f711133364a3ba79af4055d21e3",
+    "url": "./static/media/QRPlaceholder.72950f71.svg"
+  },
+  {
+    "revision": "79d274c7cb2280246c0e9ab557a31a1f",
+    "url": "./static/media/close.79d274c7.svg"
+  },
+  {
+    "revision": "cc3e92b43d95bdc7e046cc6801a89acb",
+    "url": "./static/media/info.cc3e92b4.svg"
+  }
+]);
